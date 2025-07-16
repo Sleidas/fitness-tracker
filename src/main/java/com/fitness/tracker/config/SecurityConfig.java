2f1bf878ba2097b77ef8bd1bf491c4c7a78fd6d8
Work Shop <workshop@fogartybelting.com>
2025-07-16 18:03:08 +0100

    Added layout/structure for homepage/dashboard

--- src/main/java/com/fitness/tracker/config/SecurityConfig.java
+++ src/main/java/com/fitness/tracker/config/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.fitness.tracker.config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -13,8 +23,8 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> auth
-				// Allow anyone to access the home page, login, register, and static resources
-				.requestMatchers("/", "/login", "/register", "/images/**", "/css/**", "/h2-console/**").permitAll()  //css so styles load in even before log in
+				// Allow anyone to access login, register, and static resources
+				.requestMatchers("/register", "/login","/images/**", "/css/**", "/h2-console/**").permitAll()  //css so styles load in even before log in
 				// All other requests need authentication
 				.anyRequest().authenticated()).formLogin(form -> form
 						// login page URL
@@ -40,4 +50,5 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 		
 	}
+	
 }
--- src/main/java/com/fitness/tracker/controller/HomeController.java
+++ src/main/java/com/fitness/tracker/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.fitness.tracker.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,14 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Welcome to My Fitness Tracker");
+        
+        // Get logged-in user name from Spring Security
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName(); // Usually returns username or email
+
+        // Add username to model
+        model.addAttribute("username", username);
+        
         return "home";
     }
     
